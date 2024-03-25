05844f179f31f39a1610e9a3ec23fd753bda3814
odogancay-ku <odogancay21@ku.edu.tr>
2024-03-25 22:09:40 +0300

    Textures started

--- src/objects/graphics/Textures.cpp
+++ src/objects/graphics/Textures.cpp
@@ -1,3 +1,10 @@
 //
 // Created by ofaru on 25.03.2024.
 //
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include <complex>
+#include <glm/geometric.hpp>
+#include "ResourceManager.h"
